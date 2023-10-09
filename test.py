6fa23316e4a8739ee545496d42094aca6c8317dd
Ariel Alves <alvzariel@gmail.com>
2023-10-09 16:56:20 -0300

    nova biblioteca em uso (fdpf)

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.platypus import Image
+import fpdf
 
-def mm2p(milimetros):
-    return milimetros / 0.352777
-
-def redimensioneImage(image_path):
-    width, height = img.wrapOn()
-    proportion = 
-
-cnv = canvas.Canvas("Relatório.pdf", pagesize=A4)
-cnv.drawCentredString(mm2p(105), mm2p(280), "Análise de dados de um conjunto de pessoas")
-cnv.drawImage("temp/genreNumber.png", mm2p(50), mm2p(200), width=mm2p(110), height=mm2p(110))
-cnv.save()
